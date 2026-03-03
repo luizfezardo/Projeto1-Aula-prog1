04f5c5146c6fa0523d632b896c36c56fe9633a24
Luiz Felipe Steilein Zardo <444418@unoesc.lan>
2026-03-02 21:35:10 -0300

    Commit alunos2

--- Projeto1/Controllers/NotasAlunos.cs
+++ Projeto1/Controllers/NotasAlunos.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Eventing.Reader;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Projeto1.Controllers
 {
     public class NotasAlunos : Controller
     {
-        public IActionResult Index()
+        public IActionResult Notas()
         {
+            Dictionary<string, double> notas = new Dictionary<string, double>();
+
+            notas.Add("João", 8.5);
+            notas.Add("Maria", 9.0);
+            notas.Add("Pedro", 7.5);
+            notas.Add("Ana", 9.5);
+            notas.Add("Lucas", 6.0);
+
+            double media = notas.Values.Average();
+            double maiorNota = notas.Values.Max();
+            double menorNota = notas.Values.Min();
+
+            foreach (var aluno in notas)
+            {
+                if (aluno.Value >= 7)
+                {
+                    Console.WriteLine($"{aluno.Key} aprovado");
+                }
+                else
+                {
+                    Console.WriteLine($"{aluno.Key} reprovado");
+                }
+            }
+
             return View();
         }
-    }
-}
