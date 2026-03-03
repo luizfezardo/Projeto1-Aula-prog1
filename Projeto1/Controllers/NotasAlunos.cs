1462f5153c4ce84f29499f4538ee1995bdd62ca9
Luiz Felipe Steilein Zardo <444418@unoesc.lan>
2026-03-02 21:34:25 -0300

    Commit alunos

---
+++ Projeto1/Controllers/NotasAlunos.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto1.Controllers
+{
+    public class NotasAlunos : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
