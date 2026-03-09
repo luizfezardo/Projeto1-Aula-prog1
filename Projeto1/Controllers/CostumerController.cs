76fa3b280f1a0a36d15ae52e7aa9d3feb9d2fa93
Luiz Felipe Steilein Zardo <444418@unoesc.lan>
2026-03-09 20:08:19 -0300

    Criado classe (Costumer)

---
+++ Projeto1/Controllers/CostumerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projeto1.Models;
+
+namespace Projeto1.Controllers
+{
+    public class CostumerController : Controller
+    {
+        public IActionResult Index()
+        {
+            Costumer costumer = new Costumer();
+
+
+            return View();
+        }
+    }
+}
---
+++ Projeto1/Models/Costumer.cs
@@ -0,0 +1,22 @@
+﻿namespace Projeto1.Models
+{
+    public class Costumer
+    {
+        public int CostumerId { get; set; }
+        public string? EmailAdress{ get; set; }
+        public string? firstName { get; set; }
+        public string? lastName { get; set; }
+        public bool Validate()
+        {
+            if (string.IsNullOrEmpty(EmailAdress)
+                || string.IsNullOrEmpty(firstName)
+                || string.IsNullOrEmpty(lastName)
+                )
+
+                return false;
+
+            return true;
+                
+           }
+        }    
+    }
